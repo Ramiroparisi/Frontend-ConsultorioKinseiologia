0e6990ad2473682cccf134cb5951d42a4f83b821
Ramiro <ramiroparisi678@gmail.com>
2024-10-15 13:27:22 -0300

    Creo formulario para login

---
+++ src/formulario/formulario.tsx
@@ -0,0 +1,66 @@
+// importante que la función Formulario empiece con mayúscula,
+// para que react la reconozca como un componente
+
+import React, { useState } from 'react';
+import './formulario.css'; // Importa los estilos personalizados
+
+// Formulario antes de bootstrap
+const Formulario = () => {
+  const [mail, setMail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleLogin = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    // Validación de los inputs
+    if (!mail || !password) {
+      setError('Todos los campos son obligatorios');
+      return;
+    }
+
+    if (!/\S+@\S+\.\S+/.test(mail)) {
+      setError('El correo electrónico no es válido');
+      return;
+    }
+
+    // La lógica de la autenticación se puede hacer acá, pero nosotros la hacemos en el backend
+    console.log('Mail:', mail);
+    console.log('Password:', password);
+
+    // Limpiar el error si la validación pasa
+    setError('');
+  };
+  return (
+    <section>
+      <div className='DivGeneral'>
+        <h1> Iniciar sesión </h1>
+        <form onSubmit={handleLogin}>
+           {error && <p style={{ color: 'red' }}>{error}</p>}
+          <div className='DivMail'>
+            <label htmlFor="mail">Mail:</label>
+            <input
+              type="email"
+              id="mail"
+              value={mail}
+// El evento onChange ayuda a almacenar el valor del input en el estado
+              onChange={(e) => setMail(e.target.value)}
+            />
+          </div>
+          <div className='DivPassword'>
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button type="submit">Ingresar</button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Formulario;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+// import App from './App.tsx'
 import './index.css'
+import Formulario from './formulario/formulario.tsx'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Formulario />
   </React.StrictMode>,
 )
