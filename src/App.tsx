ad94d7b1608439e20227525250af47c7e340b7a1
FacuMorresi <facumorresi@yahoo.com.ar>
2024-11-06 16:59:17 -0300

    agrego que funcione secretaria dashboards

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import LoginPaciente from './componentes/login/loginPaciente.tsx';
 import LoginSecretaria from './componentes/login/loginSecretaria.tsx';
 import PacienteDashboard from './dashboards/pacienteDashboard.tsx';
 import KinesiologoDashboard from './dashboards/kinesiologoDashboard.tsx';
+import SecretariaDashboard from './dashboards/secretariaDashboard.tsx';
 import SignUpPaciente from './componentes/login/signupPaciente.tsx';
 import SobreNosotros from './componentes/header/sobreNosotros.tsx';
 import Contacto from './componentes/header/contacto.tsx';
@@ -17,29 +18,116 @@ import CrearTurnoPaciente from './componentes/crearTurnos/turnoNuevoPaciente.tsx
 import DashboardHeader from './componentes/header/dashboardHeader.tsx';
 import DashboardHeaderKine from './componentes/header/dashboardHeaderKine.tsx';
 
-
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<><Header /><HomePages /></>} />
-        <Route path="/loginKinesiologo" element={<><Header /><LoginKinesiolgo /></>} />
-        <Route path="/loginPaciente" element={<><Header /><LoginPaciente /></>} />
-        <Route path="/loginSecretaria" element={<><Header /><LoginSecretaria /></>} />
-        <Route path="/signupPaciente" element={<><Header /><SignUpPaciente /></>} />
+        <Route
+          path="/"
+          element={
+            <>
+              <Header />
+              <HomePages />
+            </>
+          }
+        />
+        <Route
+          path="/loginKinesiologo"
+          element={
+            <>
+              <Header />
+              <LoginKinesiolgo />
+            </>
+          }
+        />
+        <Route
+          path="/loginPaciente"
+          element={
+            <>
+              <Header />
+              <LoginPaciente />
+            </>
+          }
+        />
+        <Route
+          path="/loginSecretaria"
+          element={
+            <>
+              <Header />
+              <LoginSecretaria />
+            </>
+          }
+        />
+        <Route
+          path="/signupPaciente"
+          element={
+            <>
+              <Header />
+              <SignUpPaciente />
+            </>
+          }
+        />
 
-         {/* Rutas para los dashboards */}
-    
-        <Route path="/pacienteDashboard" element={<><DashboardHeader /><PacienteDashboard /></>} />
-        <Route path="/kinesiologoDashboard" element={<><DashboardHeaderKine /><KinesiologoDashboard /></>} />
-        
+        {/* Rutas para los dashboards */}
+
+        <Route
+          path="/pacienteDashboard"
+          element={
+            <>
+              <DashboardHeader />
+              <PacienteDashboard />
+            </>
+          }
+        />
+        <Route
+          path="/kinesiologoDashboard"
+          element={
+            <>
+              <DashboardHeaderKine />
+              <KinesiologoDashboard />
+            </>
+          }
+        />
+        <Route
+          path="/secretariaDashboard"
+          element={
+            <>
+              <DashboardHeader />
+              <SecretariaDashboard />
+            </>
+          }
+        />
+
         {/* Rutas para el header*/}
-        <Route path="/sobreNosotros" element={<><Header /><SobreNosotros /></>} />
-        <Route path="/contacto" element={<><Header /><Contacto /></>} /> 
+        <Route
+          path="/sobreNosotros"
+          element={
+            <>
+              <Header />
+              <SobreNosotros />
+            </>
+          }
+        />
+        <Route
+          path="/contacto"
+          element={
+            <>
+              <Header />
+              <Contacto />
+            </>
+          }
+        />
 
         {/* Rutas para los turnos nuevos */}
-        <Route path="/turnoNuevoPaciente" element={<><Header /><CrearTurnoPaciente /></>} />
-        
+        <Route
+          path="/turnoNuevoPaciente"
+          element={
+            <>
+              <Header />
+              <CrearTurnoPaciente />
+            </>
+          }
+        />
       </Routes>
     </Router>
   );
@@ -48,7 +136,7 @@ const App = () => {
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
 
 export default App;
